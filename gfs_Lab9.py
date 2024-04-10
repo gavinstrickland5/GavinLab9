e6c02d32b2e43852ac9874bd78f95c31f67fbf2b
gavinstrickland5 <gavin.f.strickland@gmail.com>
2024-04-10 08:20:21 -0400

    gfs_Lab9.py

---
+++ gfs_Lab9.py
@@ -0,0 +1,15 @@
+menu_option = 0
+
+def encode(string):
+    output = ""
+    for i in string:
+        output += str(int(i) + 3)
+    return output
+
+while menu_option != 3:
+    print("Menu:\n1. Encoder\n2. Decoder\n3. Exit")
+    menu_option = int(input("What would you like to do? "))
+    if menu_option == 1:
+        encode_str = input("Input string to be encoded: ")
+        print("Your password has been encoded and stored!\n")
+print("Goodbye!")
